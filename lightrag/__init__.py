28b36b4fa700c8511e623d40baba0b47cded74e1
Benjamin Marchand <ben.marchand@free.fr>
2025-09-25 17:10:11 +0000

    chore: bump version to 1.8.2

--- lightrag/__init__.py
+++ lightrag/__init__.py
@@ -1,5 +1,5 @@
 from .lightrag import LightRAG as LightRAG, QueryParam as QueryParam
 
-__version__ = "1.8.1"
+__version__ = "1.8.2"
 __author__ = "Zirui Guo (forked by cyberbobjr)"
 __url__ = "https://github.com/cyberbobjr/LightRAG"
